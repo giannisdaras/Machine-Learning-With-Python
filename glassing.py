fc12edeeb24b1b89f904a392f338ddaea83bd8f1
John <daras.giannhs@gmail.com>
2017-03-30 16:07:31 +0300

    This commit adds gridsearch for hyperparameters optimization

--- glassing.py
+++ glassing.py
@@ -12,6 +12,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.neural_network import MLPClassifier
 from sklearn import preprocessing
+from sklearn.naive_bayes import GaussianNB
 
 df=pd.read_csv('datasets/Iris.csv')
 # print (df.describe())
@@ -19,41 +20,64 @@ X=df.iloc[:,1:4]
 y=df.iloc[:,5]
 fold_numbers=5
 
-# X=SelectKBest(chi2,k=7).fit_transform(X,y)
+X=SelectKBest(chi2,k=3).fit_transform(X,y)
 # sc=StandardScaler()
 # X=sc.fit_transform(X)	
 # X=preprocessing.scale(X)
 
 #KNN
-knnSimpleClf=KNeighborsClassifier(n_neighbors=3)
-# parameters={'n_neighbors':list(range(100))}
-clf=GridSearchCV(knnSimpleClf,parameters,cv=5)
-clf.fit(X,y)
-print (clf.best_params_)
-# print ('Knn scoring: ',cross_val_score(knnSimpleClf,X,y,cv=fold_numbers).sum()/fold_numbers)
+knnSimpleClf=KNeighborsClassifier(n_neighbors=9)
+# k=np.arange(0,80,1)+1
+# parameters={'n_neighbors':k}
+# clf=GridSearchCV(knnSimpleClf,parameters,cv=5)
+# clf.fit(X,y)
+# print (clf.best_params_)
+print ('Knn scoring: ',cross_val_score(knnSimpleClf,X,y,cv=fold_numbers).sum()/fold_numbers)
 
-
-#SVM
-svm=SVC(kernel='linear',C=1)
-print ('SVM scoring: ',cross_val_score(svm,X,y,cv=fold_numbers).sum()/fold_numbers)
-
-#Gaussian kernel
-svmGaussian=SVC(kernel='rbf',gamma=1.0,C=1.0)
-print ('Gaussian kernel scoring: ',cross_val_score(svmGaussian,X,y,cv=fold_numbers).sum()/fold_numbers)
-
-#SVM poly
-svmPoly=SVC(kernel='poly',C=1.0)
-print ('SvmPoly scoring: ',cross_val_score(svmPoly,X,y,cv=fold_numbers).sum()/fold_numbers)
+# svm=SVC()
+# Co=(0.01,1.0,10.0,100.0)
+# kerns=('linear','rbf','poly')
+# gammas=(0.01,1.0,10.0,100.0)
+# params={'C':Co,'kernel':kerns,'gamma':gammas}
+# clf=GridSearchCV(svm,params,cv=5)
+# clf.fit(X,y)
+# print (clf.best_params_)
+svm=SVC(kernel='poly',C=0.01,gamma=1.0)
+print ('SVM score: ',cross_val_score(svm,X,y,cv=fold_numbers).sum()/fold_numbers)
 
 #Forest
-forest=RandomForestClassifier(criterion='entropy',n_estimators=100,random_state=1,n_jobs=1)
+# criteria=('entropy','gini')
+# estimators=np.arange(0,100,10)+1
+# params={'criterion':criteria,'n_estimators':estimators}
+# forest=RandomForestClassifier()
+# clf=GridSearchCV(forest,params,cv=5)
+# clf.fit(X,y)
+# print (clf.best_params_)
+forest=RandomForestClassifier(criterion='entropy',n_estimators=20,n_jobs=1)
 print ('Forest scoring: ',cross_val_score(forest,X,y,cv=fold_numbers).sum()/fold_numbers)
 
-#Logistic Regression
-logr=LogisticRegression(penalty='l1',C=100)
+# Logistic Regression
+# logr=LogisticRegression()
+# penalize=('l1','l2',)
+# Co=(0.01,1.0,10.0,100.0)
+# params={'C':Co,'penalty':penalize}
+# clf=GridSearchCV(logr,params,cv=5)
+# clf.fit(X,y)
+# print (clf.best_params_)
+logr=LogisticRegression(penalty='l2',C=10)
 print ('Logistic scoring: ',cross_val_score(logr,X,y,cv=fold_numbers).sum()/fold_numbers)
 
 #MLP
-multiP=MLPClassifier(hidden_layer_sizes=(800,))
+# learning_rates=(0.001,0.01,0.1)
+# hidden_layers=((100,),(500,),(1000,))
+# params={'learning_rate_init':learning_rates,'hidden_layer_sizes':hidden_layers}
+# multiP=MLPClassifier()
+# clf=GridSearchCV(multiP,params,cv=5)
+# clf.fit(X,y)
+# print (clf.best_params_)
+multiP=MLPClassifier(learning_rate_init=0.01,hidden_layer_sizes=(100,))
 print ('MLP scoring: ',cross_val_score(logr,X,y,cv=fold_numbers).sum()/fold_numbers)
 
+#Naive Bayes
+bayesian=GaussianNB()
+print ('Bayesian naive scoring: ',cross_val_score(logr,X,y,cv=fold_numbers).sum()/fold_numbers)
